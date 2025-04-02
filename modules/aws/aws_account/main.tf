32abfc28e9679b76c65b148e2c62b041fb3047cc
Yashraj Dighe <yashraj.dighe077@gmail.com>
2025-04-02 19:06:21 +0000

    bau/add-playground-aws-account

--- modules/aws/aws_account/main.tf
+++ modules/aws/aws_account/main.tf
@@ -1,7 +1,8 @@
 resource "aws_organizations_account" "account" {
-  name      = var.account_name
-  email     = var.account_email
-  role_name = var.organization_role_name
+  name              = var.account_name
+  email             = var.account_email
+  role_name         = var.organization_role_name
+  close_on_deletion = true
 
   lifecycle {
     prevent_destroy = true
