127aee27c9873f73f6d172be75e1126cb3fcf421
Yashraj Dighe <yashraj.dighe077@gmail.com>
2025-05-03 18:40:16 +0000

    fix all the issues related to count

--- modules/aws/aws_vpc/main.tf
+++ modules/aws/aws_vpc/main.tf
@@ -1,52 +1,51 @@
 resource "aws_vpc" "this" {
-  count      = var.create_vpc ? 1 : 0
-  cidr_block = var.vpc_cidr_block
+  for_each   = var.create_vpc ? { "vpc" : var.vpc_cidr_block } : {}
+  cidr_block = each.value
 
   tags = var.tags
 }
 
 resource "aws_internet_gateway" "this" {
-  count  = var.create_vpc ? 1 : 0
-  vpc_id = aws_vpc.this[0].id
+  for_each = var.create_vpc ? { "igw" : aws_vpc.this["vpc"].id } : {}
+  vpc_id   = each.value
 
   tags = var.tags
 }
 
 resource "aws_route_table" "public" {
-  count  = var.create_vpc ? 1 : 0
-  vpc_id = aws_vpc.this[0].id
+  for_each = var.create_vpc ? { "public" : aws_vpc.this["vpc"].id } : {}
+  vpc_id   = each.value
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this[0].id
+    gateway_id = aws_internet_gateway.this["igw"].id
   }
 
   tags = var.tags
 }
 
 resource "aws_route_table_association" "public" {
-  count          = var.create_vpc ? length(aws_subnet.public) : 0
   for_each       = var.create_vpc ? aws_subnet.public : {}
   subnet_id      = each.value.id
-  route_table_id = aws_route_table.public[0].id
+  route_table_id = aws_route_table.public["public"].id
 }
 
 resource "aws_nat_gateway" "this" {
-  count         = var.create_vpc && var.create_nat_gateway == "single" ? 1 : (var.create_nat_gateway == "per_az" ? length(var.public_subnets) : 0)
-  allocation_id = aws_eip.this[count.index].id
-  subnet_id     = element(aws_subnet.public.*.id, count.index)
+  for_each      = var.create_vpc && var.create_nat_gateway != "none" ? tomap({ for idx, subnet in aws_subnet.public : idx => subnet }) : {}
+  allocation_id = aws_eip.this[each.key].id
+  subnet_id     = each.value.id
 
   tags = var.tags
 }
 
 resource "aws_eip" "this" {
-  count = var.create_vpc && var.create_nat_gateway == "single" ? 1 : (var.create_nat_gateway == "per_az" ? length(var.public_subnets) : 0)
-  vpc   = true
+  for_each = var.create_vpc && var.create_nat_gateway != "none" ? tomap({ for idx, subnet in aws_subnet.public : idx => subnet }) : {}
+  vpc      = true
 }
 
 resource "aws_route_table" "private" {
   for_each = var.create_vpc ? tomap({ for idx, cidr in var.private_subnets : idx => cidr }) : {}
-  vpc_id   = aws_vpc.this[0].id
+  vpc_id   = aws_vpc.this["vpc"].id
 
   route {
     cidr_block     = "0.0.0.0/0"
@@ -63,27 +62,24 @@ resource "aws_route_table_association" "private" {
 }
 
 resource "aws_subnet" "public" {
-  count      = var.create_vpc ? length(var.public_subnets) : 0
-  for_each   = tomap({ for idx, cidr in var.public_subnets : idx => cidr })
-  vpc_id     = aws_vpc.this[0].id
+  for_each   = var.create_vpc ? tomap({ for idx, cidr in var.public_subnets : idx => cidr }) : {}
+  vpc_id     = aws_vpc.this["vpc"].id
   cidr_block = each.value
 
   tags = var.tags
 }
 
 resource "aws_subnet" "private" {
-  count      = var.create_vpc ? length(var.private_subnets) : 0
-  for_each   = tomap({ for idx, cidr in var.private_subnets : idx => cidr })
-  vpc_id     = aws_vpc.this[0].id
+  for_each   = var.create_vpc ? tomap({ for idx, cidr in var.private_subnets : idx => cidr }) : {}
+  vpc_id     = aws_vpc.this["vpc"].id
   cidr_block = each.value
 
   tags = var.tags
 }
 
 resource "aws_subnet" "db" {
-  count      = var.create_vpc ? length(var.db_subnets) : 0
-  for_each   = tomap({ for idx, cidr in var.db_subnets : idx => cidr })
-  vpc_id     = aws_vpc.this[0].id
+  for_each   = var.create_vpc ? tomap({ for idx, cidr in var.db_subnets : idx => cidr }) : {}
+  vpc_id     = aws_vpc.this["vpc"].id
   cidr_block = each.value
 
   tags = var.tags
@@ -91,7 +87,7 @@ resource "aws_subnet" "db" {
 
 resource "aws_route_table" "db" {
   for_each = var.create_vpc ? tomap({ for idx, cidr in var.db_subnets : idx => cidr }) : {}
-  vpc_id   = aws_vpc.this[0].id
+  vpc_id   = aws_vpc.this["vpc"].id
 
   tags = var.tags
 }
