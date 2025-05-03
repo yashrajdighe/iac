cd8baa9b8f2a3414e28e021b3d98c9769016471e
Yashraj Dighe <yashraj.dighe077@gmail.com>
2025-05-03 18:51:25 +0000

    fix outputs

--- modules/aws/aws_vpc/outputs.tf
+++ modules/aws/aws_vpc/outputs.tf
@@ -4,16 +4,16 @@ output "vpc_id" {
 }
 
 output "public_subnets" {
-  value       = aws_subnet.public[*].id
+  value       = [for subnet in aws_subnet.public : subnet.id]
   description = "The IDs of the public subnets"
 }
 
 output "private_subnets" {
-  value       = aws_subnet.private[*].id
+  value       = [for subnet in aws_subnet.private : subnet.id]
   description = "The IDs of the private subnets"
 }
 
 output "nat_gateways" {
-  value       = aws_nat_gateway.this[*].id
+  value       = [for nat in aws_nat_gateway.this : nat.id]
   description = "The IDs of the NAT gateways"
 }
