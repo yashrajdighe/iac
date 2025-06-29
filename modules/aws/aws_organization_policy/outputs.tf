06161d63a5fede6d1c0b718da25d6b5323c33039
Yashraj Dighe <yashraj.dighe077@gmail.com>
2025-06-29 15:43:30 +0000

    fix outputs

--- modules/aws/aws_organization_policy/outputs.tf
+++ modules/aws/aws_organization_policy/outputs.tf
@@ -1,9 +1,9 @@
 output "id" {
-  description = "The ID of the SCP policy."
-  value       = aws_organizations_policy.this.id
+  description = "The ID of the SCP policy. Null if not enabled."
+  value       = try(aws_organizations_policy.this[0].id, null)
 }
 
 output "arn" {
-  description = "The ARN of the SCP policy."
-  value       = aws_organizations_policy.this.arn
+  description = "The ARN of the SCP policy. Null if not enabled."
+  value       = try(aws_organizations_policy.this[0].arn, null)
 }
