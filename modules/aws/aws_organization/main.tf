95eb69ab7529afe37e5d76c8a17ed129e066f6c6
Yashraj Dighe <yashraj.dighe077@gmail.com>
2025-06-29 13:17:11 +0000

    enable scp in org settings

--- modules/aws/aws_organization/main.tf
+++ modules/aws/aws_organization/main.tf
@@ -1,4 +1,5 @@
 resource "aws_organizations_organization" "org" {
   aws_service_access_principals = var.aws_service_access_principals
   feature_set                   = var.feature_set
+  enabled_policy_types          = var.enabled_policy_types
 }
