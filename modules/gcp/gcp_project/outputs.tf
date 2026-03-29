fce5ef9b88c2e6c5f4e1ee471001b5e2bd7fa208
Yashraj Dighe <yashraj.dighe077@gmail.com>
2026-03-29 22:40:32 +0530

    resolve plan error

--- modules/gcp/gcp_project/outputs.tf
+++ modules/gcp/gcp_project/outputs.tf
@@ -1,9 +1,9 @@
 output "project_id" {
-  value       = google_project.this.project_id
+  value       = google_project.this.id
   description = "Project ID of the project"
 }
 
 output "project_number" {
-  value       = google_project.this.project_number
+  value       = google_project.this.number
   description = "Project number of the project"
 }
