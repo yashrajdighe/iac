bf73ea49a38707d5f925da2e767efe2259a94f77
Yashraj Dighe <yashraj.dighe077@gmail.com>
2026-04-30 21:21:22 +0530

    fix issue with gcp module

--- modules/gcp/gcp_project/main.tf
+++ modules/gcp/gcp_project/main.tf
@@ -1,3 +1,13 @@
+check "exactly_one_of_org_or_folder" {
+  assert {
+    condition = (
+      (var.org_id != null && var.folder_id == null) ||
+      (var.org_id == null && var.folder_id != null)
+    )
+    error_message = "Exactly one of org_id or folder_id must be set."
+  }
+}
+
 resource "google_project" "this" {
   name       = var.name
   project_id = var.project_id
