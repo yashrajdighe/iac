a3442fda92d2aecc58d84af6ab0832e756a4fb62
Yashraj Dighe <yashraj.dighe077@gmail.com>
2026-05-02 00:42:11 +0530

    update default permissions of projects and folders in gcp

--- modules/gcp/gcp_folder/main.tf
+++ modules/gcp/gcp_folder/main.tf
@@ -2,3 +2,25 @@ resource "google_folder" "this" {
   display_name = var.name
   parent       = var.parent
 }
+
+locals {
+  bootstrap_admin_roles = toset([
+    "roles/owner",
+    "roles/resourcemanager.folderAdmin",
+    "roles/resourcemanager.projectIamAdmin",
+  ])
+  bootstrap_admin_bindings = {
+    for pair in setproduct(var.bootstrap_admin_members, local.bootstrap_admin_roles) :
+    "${pair[0]}|${pair[1]}" => {
+      member = pair[0]
+      role   = pair[1]
+    }
+  }
+}
+
+resource "google_folder_iam_member" "bootstrap_admin" {
+  for_each = local.bootstrap_admin_bindings
+  folder   = google_folder.this.name
+  role     = each.value.role
+  member   = each.value.member
+}
--- terragrunt/infrastructure/gcp/yashrajdighe-dev-org/management/projects/test-iac-project/terragrunt.hcl
+++ terragrunt/infrastructure/gcp/yashrajdighe-dev-org/management/projects/test-iac-project/terragrunt.hcl
@@ -22,4 +22,6 @@ inputs = {
   name       = "just-another-test-project"
   project_id = "just-another-test-project-123"
   folder_id  = dependency.gcp_folder.outputs.id
+
+  bootstrap_admin_members = ["user:yashrajdighe.dev@gmail.com"]
 }
